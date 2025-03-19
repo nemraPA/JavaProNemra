c600bdf0590fd0152e879548f6f6185df526d377
nemraPA <gloriyaarmen@gmail.com>
2025-03-19 18:14:24 +0200

    Добавил несколько строк кода

--- src/com/gmail/gloriyaarmen/homeWorks/homeWork_01/app/Main.java
+++ src/com/gmail/gloriyaarmen/homeWorks/homeWork_01/app/Main.java
@@ -2,4 +2,14 @@ package com.gmail.gloriyaarmen.homeWorks.homeWork_01.app;
 
 public class Main {
     private static final double CONV_K = 2.20462;
+
+    public static void main(String[] args) {
+        System.out.println("Converter App");
+
+        double kgs = 5;
+        double pounds = convKgsPounds(kgs);
+        System.out.println("Result is " + pounds + "pounds");
+
+    }
+
 }
