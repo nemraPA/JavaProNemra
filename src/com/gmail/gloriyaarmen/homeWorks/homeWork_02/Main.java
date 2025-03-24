278ba77d37c43f6889a54790c520bbcdd3259bd4
nemraPA <gloriyaarmen@gmail.com>
2025-03-24 14:45:02 +0200

    Изменение кода на конвертер мили в километры

---
+++ src/com/gmail/gloriyaarmen/homeWorks/homeWork_02/Main.java
@@ -0,0 +1,26 @@
+package com.gmail.gloriyaarmen.homeWorks.homeWork_02;
+
+public class Main {
+    private static final double CONV_M = 0.6214;
+
+    public static void main(String[] args) {
+        System.out.println("App for measures converting.");
+        System.out.println("Version 2.0");
+
+        double klm = 5;
+        double mil = 12;
+        double miles = convKlmMiles(klm);
+        double kilometer = convMilesKlm(mil);
+        System.out.println("Result is " + miles + " miles and "
+                + kilometer + " klm.");
+
+    }
+
+    private static double convKlmMiles(double klm) {
+        return klm * CONV_M;
+    }
+
+    private static double convMilesKlm(double mil) {
+        return mil / CONV_M;
+    }
+}
