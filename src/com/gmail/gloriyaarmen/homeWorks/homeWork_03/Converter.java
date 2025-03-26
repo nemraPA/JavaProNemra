74834b58155357ee47c4219a926551d17c554307
nemraPA <gloriyaarmen@gmail.com>
2025-03-26 12:26:59 +0200

    Д/З 03 вывод информации в консоль

--- src/com/gmail/gloriyaarmen/homeWorks/homeWork_03/Converter.java
+++ src/com/gmail/gloriyaarmen/homeWorks/homeWork_03/Converter.java
@@ -5,20 +5,5 @@ public class Converter {
     public static void main(String[] args) {
         System.out.println("App for measures converting.");
         System.out.println("Version 3.0");
-
-        double klm = 5;
-        double mil = 12;
-        double miles = convKlmMiles(klm);
-        double kilometer = convMilesKlm(mil);
-        System.out.printf("Result is %4.2f miles  and  %4.2f klm.",
-                miles, kilometer);
-    }
-
-    private static double convKlmMiles(double klm) {
-        return (9/5 * klm) + 32;
-    }
-
-    private static double convMilesKlm(double mil) {
-        return (mil - 32) / 1.8;
     }
 }
