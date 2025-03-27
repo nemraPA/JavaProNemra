f2852e4540730be706250eb5b196fe88ff8417c3
nemraPA <gloriyaarmen@gmail.com>
2025-03-27 15:58:04 +0200

    Д/З 04 выполнено

--- src/com/gmail/gloriyaarmen/homeWorks/homeWork_04/Customer.java
+++ src/com/gmail/gloriyaarmen/homeWorks/homeWork_04/Customer.java
@@ -1,4 +1,20 @@
 package com.gmail.gloriyaarmen.homeWorks.homeWork_04;
 
 public class Customer {
+
+    public final String name;
+    public final String phone;
+
+    public Customer(String name, String phone) {
+        this.name = "Tom";
+        this.phone = "555 123-8596";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
 }
--- src/com/gmail/gloriyaarmen/homeWorks/homeWork_04/Main.java
+++ src/com/gmail/gloriyaarmen/homeWorks/homeWork_04/Main.java
@@ -1,4 +1,22 @@
 package com.gmail.gloriyaarmen.homeWorks.homeWork_04;
 
 public class Main {
+    public static void main(String[] args) {
+        Customer customer = getCustomer(getData());
+        String output = "Customer: " + customer.getName() +
+                ", phone " + customer.getPhone();
+        getOutput(output);
+    }
+
+    public static String[] getData() {
+        return new String[2];
+    }
+
+    public static Customer getCustomer(String[] data) {
+        return new Customer(data[0], data[1]);
+    }
+
+    public static void getOutput(String output) {
+        System.out.println(output);
+    }
 }
