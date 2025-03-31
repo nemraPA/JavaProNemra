22c46faea6fa01aced39eb0129f7a9cb6b0d20ba
nemraPA <gloriyaarmen@gmail.com>
2025-03-31 16:30:50 +0300

    Д/З 07 Задание работает, но, неразобрался как сработал str.replace и 11 строка совсем непонятна мне.

--- src/com/gmail/gloriyaarmen/homeWorks/homeWork_07/Corrector.java
+++ src/com/gmail/gloriyaarmen/homeWorks/homeWork_07/Corrector.java
@@ -1,4 +1,15 @@
 package com.gmail.gloriyaarmen.homeWorks.homeWork_07;
 
 public class Corrector {
+
+    public String handleData(String[] strs) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int count = 0;
+        for (String str : strs) {
+            str = str.replace("b", "o");
+            count++;
+            stringBuilder.append(count).append(") ").append(str).append("\n");
+        }
+        return stringBuilder.toString();
+    }
 }
