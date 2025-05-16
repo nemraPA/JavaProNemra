5dedebcf13dfd1cb1185cf9190a33bf1ebbed779
nemraPA <gloriyaarmen@gmail.com>
2025-05-16 12:59:47 +0300

    .charAt - взять символ и прибавить к строке, получится текст задом на перед. Если строка прибавить .charAt, текст строки останется в нормальном направлении.

--- src/com/gmail/gloriyaarmen/lessons/lesson_01/Lesson_01.java
+++ src/com/gmail/gloriyaarmen/lessons/lesson_01/Lesson_01.java
@@ -1,5 +1,17 @@
 package com.gmail.gloriyaarmen.lessons.lesson_01;
 
 public class Lesson_01 {
+    public static void main(String[] args) {
 
+        System.out.println(reverse("Hello world!)"));
+    }
+
+    private static String reverse(String line) {
+        String result = "";
+        for (int i = 0; i < line.length(); i++) {
+            result = line.charAt(i) + result;
+//            result = result + line.charAt(i);
+        }
+        return result;
+    }
 }
